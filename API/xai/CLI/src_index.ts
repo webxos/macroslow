70bdd5a3c3f46919af3b9642e8d18b6d0beb27a4
webXOS <eefeft@gmail.com>
2026-02-27 20:49:55 -0500

    Delete API directory

--- API/xai/CLI/src_index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Sample script to test Grok CLI integration
- */
-async function main() {
-  console.log('Grok CLI test script');
-  // Run this with: grok -p "run src/index.ts" -d .
-}
-
-main();
